0763a620ecb748cb98827e27e9bd3dd433164aa9
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-30 21:47:23 +0530

    Add footer component to the homepage

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,9 +1,23 @@
+import Footer from "../footer/Footer";
 import Header from "../header/Header";
+import Companies from "../landingPage/Companies";
+import DreamJob from "../landingPage/dream-job";
+import JobCategory from "../landingPage/job-category";
+import Subscribe from "../landingPage/Subscribe";
+import Testimonials from "../landingPage/Testimonials";
+import Working from "../landingPage/Working";
 
 const HomePage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
       <Header />
+      <DreamJob />
+      <Companies />
+      <JobCategory />
+      <Working />
+      <Testimonials />
+      <Subscribe />
+      <Footer />
     </div>
   );
 };
