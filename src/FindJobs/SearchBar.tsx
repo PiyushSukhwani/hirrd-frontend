79f62477355688548ce858da6adc89d2d0f4c776
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-31 00:03:48 +0530

    Implement Search component with RangeSlider

---
+++ src/FindJobs/SearchBar.tsx
@@ -0,0 +1,41 @@
+import { Divider, RangeSlider } from "@mantine/core";
+import { dropdownData } from "../Data/JobsData";
+import { MultiInput } from "../UI/multi-input";
+import { useState } from "react";
+
+const SearchBar = () => {
+  const [value, setValue] = useState<[number, number]>([1, 100]);
+  return (
+    <div className="flex px-5 py-8">
+      {dropdownData.map((item, index) => (
+        <>
+          <div key={index} className="w-1/5">
+            <MultiInput optionsData={item} />
+          </div>
+          <Divider size="xs" orientation="vertical" mr="xs" />
+        </>
+      ))}
+      <div className="w-1/5 [&_.mantine-Slider-label]:!translate-y-10">
+        <div className="flex text-sm justify-between">
+          <div>Salary</div>
+          <div>
+            &#8377;{value[0]} LPA - &#8377;{value[1]} LPA
+          </div>
+        </div>
+        <RangeSlider
+          size={"xs"}
+          color="brightSun.4"
+          value={value}
+          onChange={setValue}
+          labelTransitionProps={{
+            transition: "skew-down",
+            duration: 150,
+            timingFunction: "linear",
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
