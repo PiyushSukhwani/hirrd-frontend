a5c5672d17d8d0d41820980f6d0a3b1ba30e04d8
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2024-12-31 00:03:11 +0530

    Implement Sort and MultiInput search components

---
+++ src/UI/Sort.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Button, Combobox, useCombobox, Text, Box } from "@mantine/core";
+import { IconAdjustments } from "@tabler/icons-react";
+
+const opt = [
+  "Relevance",
+  "Most Recent",
+  "Salary (Low to High)",
+  "Salary (High to Low)",
+];
+
+function Sort() {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  const options = opt.map((item) => (
+    <Combobox.Option value={item} key={item}>
+      {item}
+    </Combobox.Option>
+  ));
+
+  return (
+    <Combobox
+      store={combobox}
+      width={250}
+      position="bottom-start"
+      withArrow
+      onOptionSubmit={(val) => {
+        setSelectedItem(val);
+        combobox.closeDropdown();
+      }}
+    >
+      <Combobox.Target>
+        <Button onClick={() => combobox.toggleDropdown()}>Pick item</Button>
+        <div className="border border-bright-sun-400">
+            {selectedItem} <IconAdjustments />
+        </div>
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Options>{options}</Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
+  );
+}
+
+export default Sort
