1b28ef040e1b017cf81a8001a41a435831b37e27
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2025-01-01 21:46:58 +0530

    Add SearchBar component

--- src/FindTalent/SearchBar.tsx
+++ src/FindTalent/SearchBar.tsx
@@ -1,13 +1,20 @@
-import { Divider, RangeSlider } from "@mantine/core";
-import { dropdownData } from "../Data/JobsData";
+import { Divider, Input, RangeSlider } from "@mantine/core";
 import { MultiInput } from "../UI/multi-input";
 import { useState } from "react";
+import { searchFields } from "../Data/TalentData";
+import { IconUserCircle } from "@tabler/icons-react";
 
 const SearchBar = () => {
   const [value, setValue] = useState<[number, number]>([1, 100]);
   return (
-    <div className="flex px-5 py-8 items-center">
-      {dropdownData.map((item, index) => (
+    <div className="flex px-5 py-8 items-center !text-mine-shaft-100">
+      <div className="flex items-center">
+        <div className="text-bright-sun-400 bg-mine-shaft-900 rounded-full p-1 mr-2">
+          <IconUserCircle size={20} />
+        </div>
+        <Input className="[&_input]:!placeholder-mine-shaft-300" variant="unstyled" placeholder="Talent Name" />
+      </div>
+      {searchFields.map((item, index) => (
         <>
           <div key={index} className="w-1/5">
             <MultiInput optionsData={item} />
